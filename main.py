3fcf85355829214f582c352c80d98c3397ca9eb0
rovst <loganliunz@gmail.com>
2025-07-28 19:15:53 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-#flask --app main.py run
-
 print("hello world")
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL']='sqlite:///data.db'
-db=SQLAlchemy(app)
 
-class data(db.model):
-    id=db.month1(db.Interger, primary_key=True)
-    id=db.month2(db.Interger, primary_key=True)
-    id=db.month3(db.Interger, primary_key=True)
-    id=db.month4(db.Interger, primary_key=True)
-    id=db.month5(db.Interger, primary_key=True)
-    id=db.month6(db.Interger, primary_key=True)
-    id=db.month7(db.Interger, primary_key=True)
-    id=db.month8(db.Interger, primary_key=True)
-    id=db.month9(db.Interger, primary_key=True)
-    id=db.month10(db.Interger, primary_key=True)
-    id=db.month11(db.Interger, primary_key=True)
-    id=db.month12(db.Interger, primary_key=True)
+data = []
 
-    def __data__(self):
-        return'<Task %r>' % self.id
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        months = [1,2,3,4,5,6,7,8,9,10,11,12]
+        entry = {month: int(request.form[str(month)]) for month in months}
+        data.append(entry)
+        print(entry)
     return render_template('index.html')
-
-if __name__ =="main":
-    app.run(debug=True)
