f060d99d6537ba34e1ab471bd0dd41082a2b4ccd
rovst <loganliunz@gmail.com>
2025-07-28 19:54:07 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 print("hello world")
 from flask import Flask, render_template, request
-
+from models.average import average
+from data import data
 app = Flask(__name__)
 
-data = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        months = [1,2,3,4,5,6,7,8,9,10,11,12]
-        entry = {month: int(request.form[str(month)]) for month in months}
+        months = [str(i) for i in range(1, 13)]
+        entry = [int(request.form[month]) for month in months]
+
         data.append(entry)
-        print(entry)
+        #print(entry)
+
+        average()
     return render_template('index.html')
