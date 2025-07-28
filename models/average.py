98b6de982872c98ddda3cff5c7cec6e210544db5
rovst <loganliunz@gmail.com>
2025-07-28 19:53:22 +1200

    Update average.py

--- models/average.py
+++ models/average.py
@@ -1,1 +1,9 @@
+from data import data
 
+def average():
+    if not data:
+        return 0
+    a = data[-1]  # last submitted list of ints
+    avg = sum(a) / len(a)
+    print("Average:", avg)
+    return avg
