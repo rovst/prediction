a4c466069e9fda916dac1490f8f82a253a107f83
rovst <loganliunz@gmail.com>
2025-07-29 20:41:24 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-print("hello world")
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from models.average import average
 from data import data
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         months = [str(i) for i in range(1, 13)]
         entry = [int(request.form[month]) for month in months]
-
         data.append(entry)
 
-        avg=average()
-        #print(entry)
+        avg = average()
+        return redirect(url_for('output', avg=avg))
 
-        average()
     return render_template('index.html')
 
 @app.route('/output')
 def output():
-    avg= request.args.get('avg', default=0, type=float)
-    return render_template('output.html', mesage=avg)
+    avg = request.args.get('avg', default=0, type=float)
+    return render_template('output.html', message=avg)
